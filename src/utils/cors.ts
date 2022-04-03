028bc9e1548fa6b54989cf8cad8463b442aeae2d
utyfua <utyfua@gmail.com>
2022-04-03 10:55:54 +0200

    fix cors if url missing

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -1,16 +1,31 @@
+import type { Frame } from 'puppeteer';
 import {
     INewRequestInitialArgs, IRequestOptions,
 } from '../interfaces'
 import { getDomain, getOrigin } from './urls'
 
+function getFrameUrl(frame: Frame | null): string | null {
+    if (!frame) return null;
+
+    const url = frame.url();
+    if (url) return url;
+
+    const parentFrame = frame.parentFrame();
+
+    return getFrameUrl(parentFrame);
+}
+
 export function adjustRequestCorsHeaders({ originalRequest }: INewRequestInitialArgs, requestOptions: IRequestOptions) {
     const frame = originalRequest.frame();
+    const frameUrl = getFrameUrl(frame);
 
-    if (!frame || originalRequest.resourceType() !== "xhr") return;
+    if (!frameUrl || originalRequest.resourceType() !== "xhr") return;
 
-    const frameUrl = frame.url();
+    try {
+        if (getDomain(frameUrl) === getDomain(requestOptions.url)) return;
 
-    if (getDomain(frameUrl) === getDomain(requestOptions.url)) return;
-
-    requestOptions.headers['Origin'] = getOrigin(frameUrl);
+        requestOptions.headers['Origin'] = getOrigin(frameUrl);
+    } catch (error) {
+        console.error(error)
+    }
 }
