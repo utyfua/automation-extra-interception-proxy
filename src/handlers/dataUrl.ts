5bad42dd00fb0491f7aea06dc3a36f21ee45f6dc
utyfua <utyfua@gmail.com>
2022-07-11 06:50:43 +0200

    dataUrl implementation

--- src/handlers/dataUrl.ts
+++ src/handlers/dataUrl.ts
@@ -1,23 +1,24 @@
+import parseDataURL from 'data-urls';
 import {
-    AUTHOR_NEW_ISSUE_URL, NPM_PACKAGE_NAME,
     IInterceptionProxyRequest, IResponseOptions
 } from '../interfaces';
 
-
-let isNotified = false;
 export const dataUrlHandlerKey = 'dataUrl';
 export async function dataUrlHandler(request: IInterceptionProxyRequest): Promise<void | IResponseOptions> {
     const { url } = request;
     if (!url.startsWith('data:')) return;
-    if (!isNotified) {
-        isNotified = true;
-        request.logger({
-            level: 'warning',
-            message: `${NPM_PACKAGE_NAME} does not support full data url management\n` +
-                `Request will be continued by default but also you have option to override response\n` +
-                `If you really need to support data url let me know ${AUTHOR_NEW_ISSUE_URL}`,
-            meta: [url.slice(0, 25) + (url.length > 25 ? '。。。 ＋' + (url.length - 25) + ' symbols' : '')],
-        });
+
+    const dataURL = parseDataURL(url);
+    if (!dataURL) return; // the result cannot be parsed as a data: URL.
+
+    const contentType = dataURL.mimeType.toString()
+
+    return {
+        status: 200,
+        headers: {
+            'content-type': contentType
+        },
+        // contentType,
+        body: dataURL.body,
     }
-    return { continue: true }
 }
